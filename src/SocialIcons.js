6f9c5fc4056f964e2483e4ddc1e60fd0217e9dc3
Tom Kennedy <tom.kennedy017@gmail.com>
2018-03-27 19:05:12 +0100

    Small styling change on social icons

--- src/SocialIcons.js
+++ src/SocialIcons.js
@@ -10,9 +10,9 @@ export default class SocialIcons extends React.Component {
                 <h1 className="welcomeHeader">Connect With Me</h1>
                 <div className="row">
                     <div className="col-12 text-center mb-4">
-                        <SocialIcon url="https://www.linkedin.com/in/tom-kennedy-42529a15a/" color="#cacaca" />
-                        <SocialIcon url="https://github.com/tomkennedycode" color="#cacaca"/>
-                        <SocialIcon url="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=tom.kennedy017@gmail.com&su=I%27ve%20come%20from%20your%20portfolio&tf=1" color="#cacaca"/>
+                        <SocialIcon url="https://www.linkedin.com/in/tom-kennedy-42529a15a/" color="#cacaca" className="ml-1 mr-1" />
+                        <SocialIcon url="https://github.com/tomkennedycode" color="#cacaca" className="ml-1 mr-1" />
+                        <SocialIcon url="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=tom.kennedy017@gmail.com&su=I%27ve%20come%20from%20your%20portfolio&tf=1" color="#cacaca" className="ml-1 mr-1" />
                     </div>
                 </div>
             </div>
