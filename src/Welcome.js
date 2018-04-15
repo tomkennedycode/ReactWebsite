58cf92778bb3028ba0a5ce2caeef8b81e003799f
Tom Kennedy <tom.kennedy017@gmail.com>
2018-04-15 12:18:53 +0100

    Adding email to title

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,7 +7,7 @@ export default class Welcome extends React.Component {
         return (
             <div>
                 <Jumbotron className="borderBottom font">
-                    <h1 className="welcomeHeader">Tom Kennedy</h1>
+                    <h1 className="welcomeHeader">Tom Kennedy - tom.kennedy017@gmail.com</h1>
                     <hr className="my-2" />
                     <p>Hi! My name's Tom. I am a young and commited web developer with good experience inside a small web development team. Always thinking of new ideas and researching the latest technology when it comes to web development. I have worked on projects that included HTML, CSS, Javascript, React and C# in a high pressure environment.  </p>
                 </Jumbotron>
