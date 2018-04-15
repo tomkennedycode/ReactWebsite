8108a37b63bf3efa33cc6d52fa9c1f1ffc9de6a0
Tom Kennedy <tom.kennedy017@gmail.com>
2018-04-15 12:15:36 +0100

    General Changes

--- src/SocialIcons.js
+++ src/SocialIcons.js
@@ -12,7 +12,7 @@ export default class SocialIcons extends React.Component {
                     <div className="col-12 text-center mb-5">
                         <SocialIcon url="https://www.linkedin.com/in/tom-kennedy-42529a15a/" color="#cacaca" className="ml-1 mr-1" />
                         <SocialIcon url="https://github.com/tomkennedycode" color="#cacaca" className="ml-1 mr-1" />
-                        <SocialIcon url="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=tom.kennedy017@gmail.com&su=I%27ve%20come%20from%20your%20portfolio&tf=1" color="#cacaca" className="ml-1 mr-1" />
+                        <SocialIcon url="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=tom.kennedy017@gmail.com&su=I%27ve%20come%20across%20your%20portfolio&tf=1" color="#cacaca" className="ml-1 mr-1" />
                     </div>
                     <hr line="1" />
                 </div>
