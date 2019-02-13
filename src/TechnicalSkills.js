00cbc7902d80967c20da33a661f7b3458fb8f630
Tom Kennedy <tom.kennedy017@gmail.com>
2019-02-13 21:20:45 +0000

    small header change on Technical  skills

--- src/TechnicalSkills.js
+++ src/TechnicalSkills.js
@@ -5,9 +5,9 @@ export default class TechnicalSkills extends React.Component {
     render() {
         return (
             <div>
-                <h2>Technical Skills</h2>
+                <h2 className="welcomeHeader headerMargin">Technical Skills</h2>
                 <p>C#, MVC, Web Api 2, .NET Core 2, MSSQL, Version Control (Azure Dev Ops, TFS, Git), NPM, HTML, CSS, Unit Testing, IIS 7, Javascript(ES6/7), React.</p>
-                <p>This website was made using react.</p>
+                <p className="mb-3">This website was made using react.</p>
                 <hr className="my-2" />
             </div>
         );
